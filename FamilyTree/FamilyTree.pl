bff38b2872abddc8e5f347813fc78d6687ac2168
vanessa <vanessa@vanessas-MacBook-Pro.local>
2019-02-02 20:30:23 -0800

    started proj

---
+++ FamilyTree/FamilyTree.pl
@@ -0,0 +1,41 @@
+%female
+female(lydia).
+female(rebecca).
+female(cynthia).
+female(sofia).
+female(vanessa).
+female(gigi).
+female(denise).
+female(susy).
+
+%male
+male(issac).
+male(mario).
+male(daniel).
+male(martin).
+male(nicolas).
+male(fransico).
+male(tito).
+male(ezequiel).
+
+
+
+/*parent*/
+
+parent(sofia,lydia).
+parent(sofia,mario).
+
+parent(cynthia,mario).
+parent(cynthia,lydia).
+
+parent(ezequiel,lydia)
+parent(ezequiel,mario)
+
+parent(vanessa,daniel)
+parent(vanessa, sofia)
+
+parent(martin,daniel)
+parent(martin, sofia)
+
+parent(gigi,cynthia)
+parent(gigi, tito)
